7fb2544a7a5f0c5b5caf863b22698e8e97785387
inuneko6417 <sao64179060@gmail.com>
2026-06-07 00:50:58 +0900

    バリデーション追加

--- backend/app/models/recipe.rb
+++ backend/app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :ingredients, dependent: :destroy
   accepts_nested_attributes_for :ingredients
+
+  validates :title, presence: true
+  validates :ingredients, presence: { message: "を動画から取得できていません" }
 end
